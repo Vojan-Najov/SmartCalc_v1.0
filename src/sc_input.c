441ead26fb04a22e3455921a3672fa7b7adbb633
Vojan-Najov <serejah.mashichev@gmail.com>
2023-03-24 21:38:49 +0300

    fix input_term()

--- src/sc_input.c
+++ src/sc_input.c
@@ -1,18 +1,20 @@
-#include "input.h"
+#include "sc_input.h"
 #include "sc_error.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int sc_input_term(char **str) {
 	static size_t size = 256;
 	static char *line = NULL;
-	int status = SC_INPUT_OK;
-	char c = '\0';
+	char c;
 	size_t n = 0;
+	int status = SC_INPUT_OK;
 
 	if (line == NULL) {
 		line = (char *) malloc(sizeof(char) * size);
 	}
+	printf("> ");
 	while (42) {
 		c = fgetc(stdin);
 		if (n >= size - 1) {
@@ -28,10 +30,13 @@ int sc_input_term(char **str) {
 	}
 	if (line == NULL) {
 		sc_error_input_alloc();
-	} else if (feof(stdin) && n == 0) {
-		status = SC_INPUT_FAIL;
 	} else if (ferror(stdin)) {
 		sc_error_input(line);
+	} else if (feof(stdin) && n == 0) {
+		printf("exit\n");
+		status = SC_INPUT_FAIL;
+	} else if (!strcmp(line, "exit")) {
+		status = SC_INPUT_FAIL;
 	} else {
 		line[n] = '\0'; 
 	}
