3a15417071aff936a61a4b6f29a0bf84f5ffdf48
Vojan-Najov <serejah.mashichev@gmail.com>
2023-03-25 14:29:04 +0300

    add comment

--- include/sc_scanner.h
+++ include/sc_scanner.h
@@ -8,6 +8,15 @@
 #define SC_DEFINITION 3
 #define SC_BAD_EXPR -1
 
+/*
+  The scanner determines by the first two tokens what type the token input queue has.
+  It can be 
+    an expression, or
+    an assignment to a variable (if the first tokens are 'x' and '='), or
+    a function definition (if the first tokens are 'f' and '='), or
+    an error.
+*/
+
 int sc_scanner(sc_deque_t *lexems);
 
 #endif
