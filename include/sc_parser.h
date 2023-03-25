1ab639de152faa903e8acdf96fd72ca10ae472c7
Vojan-Najov <serejah.mashichev@gmail.com>
2023-03-25 16:09:52 +0300

    add comment

--- include/sc_parser.h
+++ include/sc_parser.h
@@ -3,6 +3,12 @@
 
 #include "sc_deque.h"
 
+/*
+  The parser translates the input queue of tokens written in infix notation into 
+  the output queue of tokens written in postfix notation - reverse Polish notation,
+  using the "shunting yard" algorithm.
+*/
+
 sc_deque_t *parser(sc_deque_t *lexems);
 
 #endif
